eb5c4951efebcfc87d7f3bc26a687ab327fd6992
NightFury <82142029+nightfury-dev@users.noreply.github.com>
2019-03-04 01:07:58 -0600

    starting robofriends projects showing React state changes.

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+const Card = () => {
+    return (
+        <div>
+            <img alt='card' src='' />
+            <div>
+                <h2>Jane Doe</h2>
+                <p>jane.doe@gmail.com</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
